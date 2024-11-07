2c2754527089dbddc161b89e2fd1fa2fb2c2ca9b
JonayKB <jonaykb@gmail.com>
2024-11-07 15:02:15 +0000

    feat(Plant): Added plant model
    Closes #9

---
+++ Plantas/app/Models/Plant.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $plant_id
+ * @property string $name
+ * @property Post[] $posts
+ */
+class Plant extends Model
+{
+    public $timestamps = false;
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'plant_id';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post', null, 'plant_id');
+    }
+}
