b706d5f1c90943734fd4a422ded3a8c422f47ce2
JonayKB <jonaykb@gmail.com>
2024-11-12 16:17:21 +0000

    chore(Schelude): Scheleude commented

--- Plantas/app/Console/Kernel.php
+++ Plantas/app/Console/Kernel.php
@@ -9,18 +9,19 @@ class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
+    *
+    *    protected function schedule(Schedule $schedule): void
+    *    {
+    *       $schedule->command('inspire')->hourly();
+    *    }
      */
-    protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
-    }
 
     /**
      * Register the commands for the application.
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
