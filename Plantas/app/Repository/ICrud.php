bcbfe7e4e1084026f3175173c61238e8bef96cb8
JonayKB <jonaykb@gmail.com>
2024-11-12 20:30:24 +0000

    feat(RolRepository): Added FindAll and Save

--- Plantas/app/Repository/ICrud.php
+++ Plantas/app/Repository/ICrud.php
@@ -1,7 +1,10 @@
 <?php
+
+use Illuminate\Database\Eloquent\Collection;
+
 interface ICrud{
-    public function findById($dto): object | null;
-    public function findAll(): array;
+    public function findById(int $id): object | null;
+    public function findAll(): Collection;
     public function save(object $dto): object;
     public function update(object $dto): bool;
     public function delete($id): bool;
--- Plantas/app/Repository/RolRepository.php
+++ Plantas/app/Repository/RolRepository.php
@@ -1,5 +1,49 @@
 <?php
 namespace App\Repository;
-class RolRepository{
+
+use App\Models\Rol;
+use Exception;
+use ICrud;
+use Illuminate\Database\Eloquent\Collection;
+
+class RolRepository implements ICrud{
+
+    public function findById(int $id): object | null{
+        $dto = null;
+        try {
+            $dto = Rol::on("mysql")->find($id);
+        } catch (Exception $e) {
+
+            $dto = Rol::on("sqliteLocal")->find($id);
+
+        }
+        return $dto;
+    }
+    public function findAll(): Collection{
+        $dtos = [];
+        try {
+            $dtos = Rol::on("mysql")->get();
+        } catch (Exception $e) {
+            $dtos = Rol::on("sqliteLocal")->get();
+        }
+        return $dtos;
+    }
+    public function save(object $dto): object{
+        try {
+            $dto->setConnection('mysql')->save();
+            $dto->setConnection('sqliteLocal')->save();
+        } catch (Exception $e) {
+            $dto->setConnection('sqliteLocal')->save();
+        }
+        return $dto;
+
+
+    }
+    public function update(object $dto): bool{
+
+    }
+    public function delete($id): bool{
+
+    }
 
 }
