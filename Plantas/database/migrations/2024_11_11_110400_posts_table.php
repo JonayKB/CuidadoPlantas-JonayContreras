e96e61095eddb62d7b4d816885634904678f484d
JonayKB <jonaykb@gmail.com>
2024-11-12 14:40:25 +0000

    fix(Migrations): Modifications on migrations

--- Plantas/database/migrations/2024_11_11_110400_posts_table.php
+++ Plantas/database/migrations/2024_11_11_110400_posts_table.php
@@ -11,17 +11,17 @@ return new class extends Migration {
         Schema::create('posts', function (Blueprint $table) {
             $table->id('post_id');
             $table->string('title', 255);
-            $table->text('description')->nullable();
+            $table->text('description');
             $table->foreignId('plant_id')->constrained('plants', 'plant_id')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->binary('image')->nullable();
-            $table->string('imageMimeType', 50)->nullable();
+            $table->binary('image');
+            $table->string('imageMimeType', 50);
             $table->integer('reports')->default(0);
             $table->timestamps();
         });
         DB::table('posts')->insert([
-            ['post_id' => 1, 'title' => 'First post', 'description' => 'This is the first post', 'plant_id' => 1, 'user_id' => 1, 'image' => null, 'imageMimeType' => null, 'reports' => 0],
-            ['post_id' => 2, 'title' => 'Second post', 'description' => 'This is the second post', 'plant_id' => 2, 'user_id' => 2, 'image' => null, 'imageMimeType' => null, 'reports' => 0],
+            ['post_id' => 1, 'title' => 'First post', 'description' => 'This is the first post', 'plant_id' => 1, 'user_id' => 1, 'image' => 'image', 'imageMimeType' => '.jpg', 'reports' => 0],
+            ['post_id' => 2, 'title' => 'Second post', 'description' => 'This is the second post', 'plant_id' => 2, 'user_id' => 2, 'image' => 'image', 'imageMimeType' => '.jpg', 'reports' => 0],
         ]);
     }
 
