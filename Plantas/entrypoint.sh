68419fa019af1c41d26467ce83903935542d6ec1
jonaykb <jonaykb@gmail.com>
2024-11-22 19:30:56 +0000

    ci(EntryPoint): Changed php max file size

--- Plantas/entrypoint.sh
+++ Plantas/entrypoint.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-# Wait mysql
+# Esperar a MySQL
 echo "Waiting for MySql..."
 until mysqladmin ping -h "plantas-mysql-db" --silent; do
     echo "Waiting for MySql..."
     sleep 2
 done
 
-# Composer Dependencies
+# Instalar dependencias de Composer
 if [ ! -d "/var/www/vendor" ]; then
     echo "Installing Composer..."
     composer update
     composer install
 fi
-# Node dependencies
+
+# Instalar dependencias de Node.js
 if [ ! -d "/var/www/node_modules" ]; then
   echo "Installing Node.js..."
   npm install
 fi
 
-# Compile Vite
+# Compilar Vite
 if [ ! -f "/var/www/public/build/manifest.json" ]; then
   echo "Compiling Vite..."
   npm run build
-
 fi
-# Create sqlite Database
+
+# Crear la base de datos SQLite
 if [ ! -f "/var/www/database/database.sqlite" ]; then
     echo "Creating SQLite database..."
     touch /var/www/database/database.sqlite
@@ -33,13 +34,18 @@ if [ ! -f "/var/www/database/database.sqlite" ]; then
     chown www-data:www-data /var/www/database/database.sqlite
     php /var/www/artisan migrate --database sqliteLocal
 fi
+if [ ! -f "/usr/local/etc/php/conf.d/uploads.ini" ]; then
+# Cambiar los valores de upload_max_filesize y post_max_size a 100M
+echo "Setting upload_max_filesize and post_max_size to 100M"
+echo "upload_max_filesize = 100M" >> /usr/local/etc/php/conf.d/uploads.ini
+echo "post_max_size = 100M" >> /usr/local/etc/php/conf.d/uploads.ini
+fi
 
-
-# Execute Migrations
+# Ejecutar migraciones
 echo "Doing migrations..."
 php /var/www/artisan migrate
 php artisan key:generate
 
-# Start PHP-FPM server
+# Iniciar PHP-FPM
 echo "Starting PHP-FPM..."
 php-fpm
